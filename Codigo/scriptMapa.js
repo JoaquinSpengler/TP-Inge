94746a548b0adaf5efbe4e34cf30427ab129457e
Joaquin Spengler <juacospengler@gmail.com>
2024-05-31 12:36:19 -0300

    emprendimientos con direcciones reales para mostrar, solo falta mostrarlo en el mapa

--- Codigo/scriptMapa.js
+++ Codigo/scriptMapa.js
@@ -1,7 +1,8 @@
    // Inicializar el mapa
-   const map = L.map('map').setView([-34.5361969734694,-58.7107224346939], 13);
+   const map = L.map('map').setView([-34.5361969734694 + 0.03,-58.7107224346939], 13);
    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
        maxZoom: 19,
        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-   }).addTo(map);
-
+   }).addTo(map)
+   
+   var marker = L.marker([-34.5361969734694,-58.7107224346939]).addTo(map);
