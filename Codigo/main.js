e69cfb10ddbc4d0dc8a0a2d15c70f90246983b63
Joaquin Spengler <juacospengler@gmail.com>
2024-05-31 13:27:22 -0300

    el mapa muestra las direcciones del emprendimiento al desplegar la carta

--- Codigo/main.js
+++ Codigo/main.js
@@ -11,12 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
                     <h2>${emprendimiento.nombre}</h2>
                     <p class="summary">${emprendimiento.descripcion}</p>
                     <div class="extra-info">
-                        <p><strong>Dirección:</strong> ${emprendimiento.direccion}</p>
+                        <p><strong>Dirección:</strong> ${emprendimiento.direccion_emprendimiento}</p>
                         <p><strong>Teléfono:</strong> ${emprendimiento.telefono}</p>
                         <p><strong>Rubro:</strong> ${emprendimiento.rubro}</p>
+                    </div>
                 `;
                 card.addEventListener("click", function() {
-                    toggleCard(this);
+                    toggleCard(this, emprendimiento);
                 });
                 cardsContainer.appendChild(card);
             });
@@ -24,10 +25,12 @@ document.addEventListener("DOMContentLoaded", function() {
         .catch(error => {
             console.error("Error al cargar los emprendimientos:", error);
         });
-
- 
 });
 
-function toggleCard(element) {
+function toggleCard(element, emprendimiento) {
     element.classList.toggle('expanded');
+    if (emprendimiento.mostrarDireccion) {
+        const latLng = emprendimiento.posicion_emprendimiento;
+        setMapView(latLng);
+    }
 }
--- Codigo/scriptMapa.js
+++ Codigo/scriptMapa.js
@@ -1,8 +1,12 @@
+
    // Inicializar el mapa
-   const map = L.map('map').setView([-34.5361969734694 + 0.03,-58.7107224346939], 13);
-   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       maxZoom: 19,
-       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-   }).addTo(map)
-   
-   var marker = L.marker([-34.5361969734694,-58.7107224346939]).addTo(map);
+const map = L.map('map').setView([-34.5361969734694, -58.7107224346939], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function setMapView(latLng) {
+    map.setView(latLng, 13);
+    L.marker(latLng).addTo(map);
+}
